9132243f23f3b6508775df9cf666e1e97ae65695
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-07-07 07:32:30 +0100

    exec

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 """
-This module adds all arguments to a Python list
-then save them to a file
+This program take the file add_item.json, and add the
+parameters to the list inside this file.
+- If the file doesn't exist create it.
+- If no exist parameters do nothing or create the list if the file is empty.
 """
 
-
-import json
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+from os.path import exists
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-try:
-    obj = load_from_json_file('add_item.json')
-    save_to_json_file(obj + argv[1:], 'add_item.json')
-except Exception as e:
-    save_to_json_file(argv[1:], 'add_item.json')
+namefile = "add_item.json"
+argc = len(argv)
+
+file_list = []
+
+if exists(namefile):
+    file_list = load_from_json_file(namefile)
+
+if (argc == 1):
+    save_to_json_file(file_list, namefile)
+else:
+    for index in range(1, argc):
+        file_list.append(argv[index])
+    save_to_json_file(file_list, namefile)
