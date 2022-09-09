ceb46dc4a4f105ebf04a2cb61fbd15437f0f3429
AjabiOladipupo <dipoajabisam95@gmail.com>
2022-09-09 07:12:40 +0100

    object relational mapping

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+2. Filter states by user input
+"""
+if __name__ == "__main__":
+    """
+    Main Method
+    """
+    import MySQLdb
+    import sys
+    state = str(sys.argv[4])
+    conn = MySQLdb.connect(host='localhost',
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8"
+                           )
+    cur = conn.cursor()
+    sql = """SELECT * FROM states WHERE name LIKE BINARY '{}'
+          ORDER BY id""".format(state)
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
